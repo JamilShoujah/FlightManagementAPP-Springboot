60b6c12a38cda2733e371f18cc3c3d1a300a6741
JamilShoujah <jws09@mail.aub.edu>
2026-02-05 13:15:36 +0200

    initial commit

---
+++ src/main/java/jamil/FlightFoodService/FlightFoodServiceApplication.java
@@ -0,0 +1,13 @@
+package jamil.FlightFoodService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFoodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFoodServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jamil/FlightFoodService/FlightFoodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jamil.FlightFoodService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightFoodServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
